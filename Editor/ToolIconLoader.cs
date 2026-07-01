751bae024a9bfa963f0dba54307aa1e87cf8cea1
claynimmo <nimmo.clay@gmail.com>
2026-07-01 14:24:38 +1000

    Attempt to fix folder path #2 (I cannot test this until pushing the code, hence the multiple commits,, since it needs to be pushed to the public repo to import into another project)

--- Editor/ToolIconLoader.cs
+++ Editor/ToolIconLoader.cs
@@ -5,25 +5,21 @@ using System.IO;
 public static class ToolIconLoader
 {
     public static Texture2D LoadIcon(string relativePath){
-        // get the path to this script
         var script = MonoScript.FromScriptableObject(
             ScriptableObject.CreateInstance<BelzierPathEditorTool>()
         );
 
-        string scriptPath = AssetDatabase.GetAssetPath(script); 
-
+        string scriptPath = AssetDatabase.GetAssetPath(script);
         // Assets/EditorToolsPackage/Editor/Tools/BelzierPathEditorTool.cs
-        // Packages/EditorToolsPackage/Editor/Tools/BelzierPathEditorTool.cs
+        // Packages/com.clay.editor/Editor/Tools/BelzierPathEditorTool.cs
 
         string scriptDir = Path.GetDirectoryName(scriptPath).Replace("\\", "/");
+        string packageRoot = Path.GetDirectoryName(
+            Path.GetDirectoryName(scriptDir)
+        ).Replace("\\", "/");
 
-        // go up folders
-        string packageRoot = Path.GetFullPath(Path.Combine(scriptDir, "../.."))
-            .Replace("\\", "/");
+        string iconPath = packageRoot + "/" + relativePath;
 
-        string projectRoot = Application.dataPath.Replace("/Assets", "");
-        string projectRelativeRoot = packageRoot.Replace(projectRoot + "/", "");
-        string iconPath = projectRelativeRoot + "/" + relativePath;
         Texture2D icon = AssetDatabase.LoadAssetAtPath<Texture2D>(iconPath);
 
         if (icon == null)
