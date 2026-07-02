3cd45fee2b9ebb23556556c50d56539c751d0454
claynimmo <nimmo.clay@gmail.com>
2026-07-02 12:41:23 +1000

    replaced the audio source and light views with a generic special component check. This checks for all supported components, showing the one with the highest priority. Turned it off by default, as to not clutter the menu. Adjusted the check for invisible objects to require a collider of some kind, as otherwise it would render for every component with an unsupported component type, cluttering the menu. So, the invisible icon now only represents invisible walls.

--- Editor/Hierarchy/Visuals/HierarchyIcons.cs
+++ Editor/Hierarchy/Visuals/HierarchyIcons.cs
@@ -1,6 +1,9 @@
 
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
 
 [InitializeOnLoad]
 public static class HierarchyIcons
@@ -11,8 +14,43 @@ public static class HierarchyIcons
     static Texture2D invisibleObjectIcon;
     static Texture2D colliderIcon;
     static Texture2D customScriptIcon;
+    static Texture2D cameraIcon;
 
-    private static int _horizontalOffset = 16;
+    private static readonly int _horizontalOffset = 16;
+
+    // a set of supported components with unique properties, indexed by rarity. Higher rarity is shown over a component with lower rarity
+    // only includes components that are useful to see. (you would never need to visually see the audio listener, for example)
+    private static readonly Dictionary<Type,int> _componentRarities = new(){
+
+        // 100 series: common, but useful to see components
+        {typeof(Light),1},
+        {typeof(AudioSource),2},
+        {typeof(Camera),3},
+        {typeof(Animator),4},
+
+        // 200 series: uncommon objects
+        {typeof(Terrain),200},
+
+        // 400 series: effects
+        {typeof(ParticleSystem),400},
+        {typeof(TrailRenderer),401},
+        {typeof(LineRenderer),402},
+        {typeof(ParticleSystemForceField),403},
+        {typeof(LensFlare),404},
+
+        // 500 series: UI
+        {typeof(Canvas),550},
+        {typeof(CanvasGroup),551},
+        {typeof(Toggle),501},
+        {typeof(UnityEngine.UI.Button),502},
+        {typeof(Slider),503},
+        {typeof(ScrollRect),504},
+        {typeof(Scrollbar),505},
+
+
+        // 1000 series: one per scene
+        {typeof(ReflectionProbe),1000},
+    };
 
     static HierarchyIcons(){
         ColorUtility.TryParseHtmlString("#ffb700c2", out Color audioCol);
@@ -29,6 +67,8 @@ public static class HierarchyIcons
 
         lightIcon = MakeReadable(EditorGUIUtility.IconContent("PointLight Gizmo").image as Texture2D);
         lightIcon = TintTexture(lightIcon, lightCol);
+
+        cameraIcon = MakeReadable(EditorGUIUtility.IconContent("d_Camera Icon").image as Texture2D);
 
         invisibleObjectIcon = TintTexture(defaultIcon, invisibleCol);
         emptyObjectIcon = TintTexture(defaultIcon, emptyCol);
@@ -49,15 +89,14 @@ public static class HierarchyIcons
 
         if(HierarchyIconsPreferences.ShowEmpty && CheckEmpty(obj,rect))  return;
 
-        if(HierarchyIconsPreferences.ShowAudio && CheckAudio(obj, rect)) return;
-
-        if(HierarchyIconsPreferences.ShowLight && CheckLight(obj, rect)) return;
+        if(HierarchyIconsPreferences.ShowSpecial && CheckComponent(obj,rect)) return;
 
         if(HierarchyIconsPreferences.ShowScript && CheckScript(obj,rect))return;
 
+        if(HierarchyIconsPreferences.ShowInvisible && CheckInvisible(obj,rect)) return;
+
         if(HierarchyIconsPreferences.ShowCollider && CheckCollider(obj, rect)) return;
 
-        if(HierarchyIconsPreferences.ShowInvisible && CheckInvisible(obj,rect)) return;
     }
 
     // a function per check, so reorganising the priority can be made easily in the ongui function
@@ -71,26 +110,9 @@ public static class HierarchyIcons
 
     static bool CheckInvisible(GameObject obj, Rect rect){
         Renderer rend = obj.GetComponent<Renderer>();
-        if(rend==null || !rend.enabled){
+        Collider col = obj.GetComponent<Collider>();
+        if((rend==null || !rend.enabled) && (col != null)){
             PaintIcon(invisibleObjectIcon, obj, rect, new Vector2(4,1));
-            return true;
-        }
-        return false;
-    }
-
-    static bool CheckAudio(GameObject obj, Rect rect){
-        AudioSource source = obj.GetComponent<AudioSource>();
-        if(source != null){
-            PaintIcon(audioIcon, obj, rect, new Vector2(4,0));
-            return true;
-        }
-        return false;
-    }
-
-    static bool CheckLight(GameObject obj, Rect rect){
-        Light l = obj.GetComponent<Light>();
-        if(l != null){
-            PaintIcon(lightIcon, obj, rect, new Vector2(3f,0));
             return true;
         }
         return false;
@@ -105,19 +127,70 @@ public static class HierarchyIcons
         return false;
     }
 
+    static bool CheckComponent(GameObject obj, Rect rect, Vector2? offset = null){
+        if(offset == null) offset = new Vector2(3, 0);
+
+        var comps = obj.GetComponents<Component>(); // get all components;
+
+        int bestRank = 0;
+        Component rarest = null;
+
+        foreach (var c in comps){
+
+            var type = c.GetType();
+            
+            if(!_componentRarities.ContainsKey(type))
+                continue;
+            
+            if(_componentRarities.TryGetValue(type, out int rank)){
+                if(rank > bestRank){
+                    bestRank = rank;
+                    rarest = c;
+                }
+            }
+            
+        }
+        if(rarest == null) return false;
+
+        Texture2D icon = GetComponentIcon(rarest);
+        if(icon == null) return false;
+
+        PaintIcon(icon,obj,rect,offset);
+
+        return true;
+    }
+
     static bool CheckScript(GameObject obj, Rect rect){
-        MonoBehaviour h = obj.GetComponent<MonoBehaviour>();
-        if(h != null){
-            PaintIcon(customScriptIcon, obj, rect, new Vector2(4,1), new Vector2(12,12));
-            return true;
+        MonoBehaviour[] h = obj.GetComponents<MonoBehaviour>();
+        foreach(MonoBehaviour c in h){
+            if(IsCustomScript(c)){
+                PaintIcon(customScriptIcon, obj, rect, new Vector2(4,1), new Vector2(12,12));
+                return true;
+            }
         }
         return false;
     }
 
-    // function to print the icon to the hiearchy
-    static void PaintIcon(Texture2D icon, GameObject obj, Rect rect, Vector2 offset, Vector2? scale = null){
+    static bool IsCustomScript(Component c){
+        string asm = c.GetType().Assembly.GetName().Name;
 
-        if(scale == null) scale = new Vector2(16,16);
+        if (asm.StartsWith("UnityEngine")) return false;
+        if (asm.StartsWith("UnityEditor")) return false;
+
+        return true;
+    }
+
+    static Texture2D GetComponentIcon(Component c){
+        GUIContent content = EditorGUIUtility.ObjectContent(null, c.GetType());
+        return content.image as Texture2D;
+    }
+
+
+    // function to print the icon to the hiearchy
+    static void PaintIcon(Texture2D icon, GameObject obj, Rect rect, Vector2? offset, Vector2? scale = null){
+
+        if(scale == null) scale   = new Vector2(16,16);
+        if(offset == null) offset = new Vector2(4,1);
 
         GUIStyle style = GUI.skin.label;
 
@@ -135,7 +208,7 @@ public static class HierarchyIcons
         else if (!obj.activeSelf)
             GUI.color = new Color(0.6f, 0.6f, 0.6f, 1f); // darkened
 
-        Rect iconRect = new Rect(x + offset.x, rect.y+offset.y, scale.Value.x, scale.Value.y);
+        Rect iconRect = new Rect(x + offset.Value.x, rect.y+offset.Value.y, scale.Value.x, scale.Value.y);
         GUI.DrawTexture(iconRect, icon);
 
         GUI.color = Color.white;
--- Editor/Hierarchy/Visuals/HierarchyIconsPreferences.cs
+++ Editor/Hierarchy/Visuals/HierarchyIconsPreferences.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 static class HierarchyIconsPreferences
 {
     private const string EnableIconsKey = "Clay_EditorTools.HierarchyIcons_Enable";
-    private const string ShowAudioKey = "Clay_EditorTools.HierarchyIcons_ShowAudio";
-    private const string ShowLightKey = "Clay_EditorTools.HierarchyIcons_ShowLight";
+    private const string ShowSpecialKey = "Clay_EditorTools.HierarchyIcons_ShowSpecial";
+
     private const string ShowEmptyKey = "Clay_EditorTools.HierarchyIcons_ShowEmpty";
     private const string ShowInvisibleKey = "Clay_EditorTools.HierarchyIcons_ShowInvisible";
     private const string ShowColliderKey = "Clay_EditorTools.HierarchyIcons_ShowCollider";
@@ -14,8 +14,7 @@ static class HierarchyIconsPreferences
     public static string RightAlignKey = "Clay_EditorTools.HierarchyIcons_RightAlign";
 
     public static bool EnableIcons => EditorPrefs.GetBool(EnableIconsKey, true);
-    public static bool ShowAudio => EditorPrefs.GetBool(ShowAudioKey, true);
-    public static bool ShowLight => EditorPrefs.GetBool(ShowLightKey, true);
+    public static bool ShowSpecial => EditorPrefs.GetBool(ShowSpecialKey, false);
     public static bool ShowEmpty => EditorPrefs.GetBool(ShowEmptyKey, true);
     public static bool ShowInvisible => EditorPrefs.GetBool(ShowInvisibleKey, true);
     public static bool ShowCollider => EditorPrefs.GetBool(ShowColliderKey, false);
@@ -30,8 +29,7 @@ static class HierarchyIconsPreferences
                 EditorGUI.BeginChangeCheck();
 
                 bool enableIcons = EditorPrefs.GetBool(EnableIconsKey, true);
-                bool showAudio = EditorPrefs.GetBool(ShowAudioKey, true);
-                bool showLight = EditorPrefs.GetBool(ShowLightKey, true);
+                bool showSpecial = EditorPrefs.GetBool(ShowSpecialKey, false);
                 bool showEmpty = EditorPrefs.GetBool(ShowEmptyKey, true);
                 bool showInvisible = EditorPrefs.GetBool(ShowInvisibleKey, true);
                 bool showCollider = EditorPrefs.GetBool(ShowColliderKey, false);
@@ -43,8 +41,7 @@ static class HierarchyIconsPreferences
 
 
                 enableIcons = EditorGUILayout.Toggle("Enable Icons", enableIcons);
-                showAudio = EditorGUILayout.Toggle("Audio Icons", showAudio);
-                showLight = EditorGUILayout.Toggle("Light Icons", showLight);
+                showSpecial = EditorGUILayout.Toggle("Special Icons (eg camera, audiosource, light)", showSpecial);
                 showEmpty = EditorGUILayout.Toggle("Empty Object Icons", showEmpty);
                 showInvisible = EditorGUILayout.Toggle("Invisible Object Icons", showInvisible);
                 showCollider = EditorGUILayout.Toggle("Collider Icons", showCollider);
@@ -56,8 +53,7 @@ static class HierarchyIconsPreferences
 
                 if (EditorGUI.EndChangeCheck()){
                     EditorPrefs.SetBool(EnableIconsKey, enableIcons);
-                    EditorPrefs.SetBool(ShowAudioKey, showAudio);
-                    EditorPrefs.SetBool(ShowLightKey, showLight);
+                    EditorPrefs.SetBool(ShowSpecialKey, showSpecial);
                     EditorPrefs.SetBool(ShowEmptyKey, showEmpty);
                     EditorPrefs.SetBool(ShowInvisibleKey, showInvisible);
                     EditorPrefs.SetBool(ShowColliderKey, showCollider);
