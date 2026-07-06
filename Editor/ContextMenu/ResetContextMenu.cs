9bd5f2a9532c3760be8a986a86b4c3b557d5e4f2
claynimmo <nimmo.clay@gmail.com>
2026-07-07 09:46:43 +1000

    Refactored to move the helper functions into its own class, to lower code reuse later.

--- Editor/ContextMenu/ResetContextMenu.cs
+++ Editor/ContextMenu/ResetContextMenu.cs
@@ -28,70 +28,14 @@ public static class ResetContextMenu {
         var newComponent = Activator.CreateInstance(type);
 
         Undo.RecordObject(target, "Reset Field");
-        object defaultValue = GetValueFromPath(newComponent, property.propertyPath);
+        object defaultValue = ContextHelpers.GetValueFromPath(newComponent, property.propertyPath);
 
-        SetValueFromPath(target, property.propertyPath, defaultValue);
+        ContextHelpers.SetValueFromPath(target, property.propertyPath, defaultValue);
 
         EditorUtility.SetDirty(target);
         property.serializedObject.ApplyModifiedProperties();
         property.serializedObject.Update();
     }
-    
-    // function to get the value of a desired field as an object reference via reflection
-    private static object GetValueFromPath(object obj, string path){
-        foreach(string section in path.Split('.')){
-            if(section == "Array") continue;
-            if (section.StartsWith("data[")){ //set the object to the correct reference in the path (obj = list -> list[a])
-                // if for example "data[a]", index = a
-                int index = int.Parse(section.Substring(5,section.Length - 6));
 
-                // move into the element at index a
-                object list = obj;
-                Type listType = list.GetType();
-
-                PropertyInfo indexer = listType.GetProperty("Item");
-                obj = indexer.GetValue(list, new object[] {index});
-            }
-            else{
-                // find the field specified by the section, and store its value
-                FieldInfo field = obj.GetType().GetField(section, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-                obj = field.GetValue(obj);
-            }
-        }
-        return obj;
-    }
-
-    private static void SetValueFromPath(object obj, string path, object value){
-        string[] sections = path.Split('.');
-        for(int i = 0; i < sections.Length; i++){
-            string section = sections[i];
-            if(section == "Array") continue;
-            if(section.StartsWith("data[")){
-                int index = int.Parse(section.Substring(5,section.Length - 6));
-
-                object list = obj;
-                Type listType = list.GetType();
-
-                PropertyInfo indexer = listType.GetProperty("Item");
-
-                object[] indexArr = new object[] {index};
-
-                if(i == section.Length - 1)
-                    indexer.SetValue(list, value, indexArr);
-                else
-                    obj = indexer.GetValue(list, indexArr);
-            }
-            else{
-                // set the value of the field
-                FieldInfo field = obj.GetType().GetField(section, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-                if (i == sections.Length - 1){
-                    field.SetValue(obj, value);
-                    return;
-                }
-                else
-                    obj = field.GetValue(obj);
-            }
-        }
-    }
 
 }
